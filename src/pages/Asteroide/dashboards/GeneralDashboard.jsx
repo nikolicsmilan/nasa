48659f616d86863e102b39dd618a7cd38d662338
nikolicsmilan <nikolicsmilan@gmail.com>
2024-08-22 20:00:17 +0200

    Generaldasgboard develop

--- src/pages/Asteroide/dashboards/GeneralDashboard.jsx
+++ src/pages/Asteroide/dashboards/GeneralDashboard.jsx
@@ -17,15 +17,48 @@ const dummydata = [
 const GeneralDashboard = ({ filteredData }) => {
   const [sumObject, setSumObject] = useState(nasa);
   const { mobiletoggle } = MyConsoleContext();
-
+  const tomb = [1, 2, 3, 4, 5, 6];
   return (
     <div
       className={`  flex flex-col w-full border-0 h-full justify-start ${
         mobiletoggle ? "opacity-50" : ""
       }`}
     >
-      <div className="flex justify-start flex-wrap m-1 border-0 border-primary  w-96 m-2">
-        <div className=" glowy-button-6 shadow-2xl w-full border-b-4 border-t-2 border-r-4 border-l-2 rounded-2xl border-black flex justify-start flex-wrap shadow-2xl">
+      <div className="flex justify-center flex-wrap m-1 border-0 border-primary  w-full m-2">
+        {tomb.map((item) => (
+          <div className=" glowy-button-6  border-b-4 border-t-2 border-r-4 border-l-2 rounded-2xl border-black flex justify-start flex-wrap shadow-2xl w-96">
+            <div className=" w-full">
+              {" "}
+              <h1 className="my-1 text-center text-primary text-lg">
+                Main data
+              </h1>
+            </div>
+            <div className="border-0 w-full flex flex-wrap ">
+              {dummydata.map((item) => (
+                <div>
+                  <SquareminConsole title={item.title} value={item.value} />
+                </div>
+              ))}
+              <div>
+                <SquareminConsole
+                  title="sum of object"
+                  value={sumObject.count}
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default GeneralDashboard;
+
+/*
+
+     <div className=" glowy-button-6  border-b-4 border-t-2 border-r-4 border-l-2 rounded-2xl border-black flex justify-start flex-wrap shadow-2xl w-96">
           <div className=" w-full">
             {" "}
             <h1 className="my-5 text-center text-primary text-lg">Main data</h1>
@@ -41,13 +74,7 @@ const GeneralDashboard = ({ filteredData }) => {
             </div>
           </div>
         </div>
-      </div>
-      <div></div>
-    </div>
-  );
-};
-
-export default GeneralDashboard;
+*/
 /*
 <div className="hidden lg:block ">
 <GlowyButton>GlowyButton1</GlowyButton>
