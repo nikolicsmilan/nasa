6b39e4d9834d29f2b6ec0320f7060fd1af2cd110
nikolicsmilan <nikolicsmilan@gmail.com>
2025-03-17 21:50:56 +0100

    feat: Integrate Morgan and update logging paths

--- backend/user-service/src/app.js
+++ backend/user-service/src/app.js
@@ -1,22 +1,33 @@
 const express = require('express');
 const cors = require('cors');
-//const morgan = require('morgan');
+const morgan = require('morgan');
+const rfs = require('rotating-file-stream');
+const path = require('path');
 
 const corsOptions = require('./config/corsOptions'); // Import CORS options
 //const authRoutes = require('./auth/authRoutes');   // Import auth routes
 //const userRoutes = require('./user/userRoutes');   // Import user routes
-//const logger = require('./utils/logger');           // Import logger
 //const api = require('./routes/api');
 
 const app = express();
 
 // Middlewares (apply before routes)
-//app.use(morgan('combined', { stream: { write: message => logger.http(message.trim()) } })); // Log HTTP requests
+// Create a rotating write stream
+const accessLogStream = rfs.createStream('access.log', {
+    interval: '1d', // Rotate daily
+    path: path.join(__dirname, '..', '..', 'logs', 'morgan') // Log directory
+  });
+
+  // Setup the logger
+app.use(morgan('combined', { stream: accessLogStream }));
 app.use(cors(corsOptions)); // Apply CORS configuration
 app.use(express.json());    // Parse JSON request bodies
 app.use(express.urlencoded({ extended: false })); // Parse URL-encoded request bodies
 
 //Routes
+app.get('/', (req, res) => {
+    res.send('Nincs World3git !');
+  });
 //app.use('/', api);
 
 module.exports = app;
