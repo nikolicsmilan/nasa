9646e9a97f4e2ba8d272c301fe9421a702c0ec31
nikolicsmilan <nikolicsmilan@gmail.com>
2025-03-20 22:43:41 +0100

    Refactor: Remove unused imports and variables in Home component

--- src/pages/Home/components/NotificationButton.jsx
+++ src/pages/Home/components/NotificationButton.jsx
@@ -1,21 +1,3 @@
-/*import React from 'react';
-import { Link } from 'react-router-dom';
-
-const NotificationButton = () => {
-  return (
-    <div className='border-0 text-center w-96'>
-      <p className="text-sm text-primary mb-2">
-        Szeretnél azonnali értesítést kapni SMS-ben, ha egy potenciálisan veszélyes aszteroida közeledik a Földhöz?
-      </p>
-      <Link to="/subscription" className="inline-block bg-success hover:scale-x-105 text-white font-bold py-2 px-4 rounded">
-        Értesítést kérek
-      </Link>
-    </div>
-  );
-};
-
-export default NotificationButton;*/
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -23,11 +5,13 @@ const NotificationButton = () => {
   return (
     <div className='border-0 text-center w-96'>
       <p className="text-md text-primary mb-2 px-5">
-        Szeretnél azonnali értesítést kapni SMS-ben, ha egy potenciálisan veszélyes aszteroida közeledik a Földhöz?
+      Szeretnél SMS-t kapni veszély esetén?
       </p>
       <Link 
         to="/subscription" 
-        className="w-80 my-5 inline-block bg-green-500 hover:bg-green-500 transition-all duration-300 text-white font-bold py-3 px-6 rounded-lg shadow-lg"
+        className="w-80 my-5 inline-block bg-green-500
+         hover:bg-green-500 transition-all duration-300
+          text-white font-bold py-3 px-6 rounded-lg shadow-lg uppercase"
       >
         Ingyenes értesítést kérek!
       </Link>
