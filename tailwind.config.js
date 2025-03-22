27847ea1cfb3eed6d95cb1bd89b5fca01c93cdab
nikolicsmilan <nikolicsmilan@gmail.com>
2025-03-22 21:33:42 +0100

    Fix: Update tailwind config and app css to fix the image loading and imports

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
             primary: "blue",
           },
           fontFamily: {
-            mono: ['monospace'],
+            mono: ['"Fira Code"', 'monospace'] ,//mono: ['"Fira Code"', 'monospace']  mono: ['monospace'],
           },
           fontSize: {
             xs: "0.563rem",
@@ -44,14 +44,14 @@ module.exports = {
               asidebg: "rgb(129, 171, 178)",
             },
             fontFamily: {
-              mono: ['monospace'],
+              mono: ['"Fira Code"', 'monospace'] 
             },
             backgroundImage: {
               gradient:
                 "linear-gradient(45deg, #082f49 1%, #075985 40%,  #cffafe 90% )",
               gradientreverse:
                 "linear-gradient(225deg, #082f49 1%, #075985 40%,  #cffafe 90% )",
-              mybackground: "url('../src/assets/images/desk.jpg')",
+              //mybackground: "url('../src/assets/images/desk.jpg')",
             },
           },
         },
@@ -69,14 +69,14 @@ module.exports = {
               asidebg: "rgb(129, 171, 178)",
             },
             fontFamily: {
-              mono: ['monospace'], 
+              mono: ['"Fira Code"', 'monospace'] 
             },
             backgroundImage: {
               gradient:
                 "linear-gradient(45deg, #1a2e05 1%, #3f6212 40%, #ecfccb 90%)",
               gradientreverse:
                 "linear-gradient(225deg, #1a2e05 1%, #3f6212 40%, #ecfccb 90%)",
-              mybackground: "url('../src/assets/images/desk.jpg')",
+            //  mybackground: "url('../src/assets/images/desk.jpg')",
             },
             boxShadow: {
               green: "10px 0px 10px 3px rgba(144, 78, 82, 0.75)",
@@ -97,14 +97,14 @@ module.exports = {
               asidebg: "rgb(129, 171, 178)",
             },
             fontFamily: {
-              mono: ['monospace'],
+              mono: ['"Fira Code"', 'monospace'] 
             },
             backgroundImage: {
               gradient:
                 "linear-gradient(45deg, #052e16 1%, #166534 40%,  #dcfce7 90% )",
               gradientreverse:
                 "linear-gradient(225deg, #052e16 1%, #166534 40%,  #dcfce7 90% )",
-              mybackground: "url('../src/assets/images/desk.jpg')",
+              //mybackground: "url('../src/assets/images/desk.jpg')",
             },
           },
         },
@@ -122,14 +122,14 @@ module.exports = {
               asidebg: "rgb(237, 237, 237)",
             },
             fontFamily: {
-              mono: ['monospace'],
+              mono: ['"Fira Code"', 'monospace'] 
             },
             backgroundImage: {
               gradient:
                 "linear-gradient(45deg, #172554 1%, #1e40af 40%,  #dbeafe 90% )",
               gradientreverse:
                 "linear-gradient(225deg, #172554 1%, #1e40af 40%,  #dbeafe 90% )",
-              mybackground: "url('../src/assets/images/desk.jpg')",
+              //mybackground: "url('../src/assets/images/desk.jpg')",
             },
           },
         },
