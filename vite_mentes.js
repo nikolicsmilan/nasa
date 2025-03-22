7a4faf52861a5755003f683ca422bcb10c24bb12
nikolicsmilan <nikolicsmilan@gmail.com>
2025-03-22 21:34:00 +0100

    Remove: Clean ups and dead code

---
+++ vite_mentes.js
@@ -0,0 +1,51 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  assetsInclude: ['**/*.JPG', '**/*.PNG'], // Hozzáadtuk a .PNG-t
+  
+  build: {
+    outDir: 'build', // Itt módosítottuk a build mappa nevét.
+  },
+})
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  assetsInclude: ['**/*.JPG', '**/*.PNG'], // Hozzáadtuk a .PNG-t
+  
+  build: {
+    outDir: 'build', // Itt módosítottuk a build mappa nevét.
+  },
+})
+
+Ez is működik
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith('.jsx')) {
+            return `assets/[name]-[hash][extname]`;
+          }
+          let extType = assetInfo.name.split('.').pop();
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'img';
+          }
+          return `assets/${extType}/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
+  },
+})
