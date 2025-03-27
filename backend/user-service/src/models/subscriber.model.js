7ccb5f350dbed84fc0ac3599d3525f995f686d0b
nikolicsmilan <nikolicsmilan@gmail.com>
2025-03-27 22:05:10 +0100

    feat: Transform subscriber model into user model

--- backend/user-service/src/models/subscriber.model.js
+++ backend/user-service/src/models/subscriber.model.js
@@ -1,24 +1,3 @@
-// models/subscriber.model.js
-const mongoose = require('mongoose');
+const axios = require('axios');
 
-const subscriberSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-  },
-  subscriptionDate: {
-    type: Date,
-    default: Date.now,
-  },
-});
-
-const Subscriber = mongoose.model('Subscriber', subscriberSchema);
-module.exports = Subscriber;
+const subscriberDatabase = require("./subscriber.mongo")
