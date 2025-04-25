ff714670882ddf7a8ce056355ca954d7f0297eb3
nikolicsmilan <nikolicsmilan@gmail.com>
2025-04-25 12:35:46 +0200

    feat: Add initial hover dropdown menu components and context (ignore unecessary debug)

--- src/hooks/use-processed-graph-data.js
+++ src/hooks/use-processed-graph-data.js
@@ -17,20 +17,20 @@ import {
 const useProcessedGraphData = (sumObject, config) => {
   const displayedData = useMemo(() => {
     const sourceData = sumObject?.data;
-    console.log(">>> DEBUG [useProcessedGraphData] sourceData:", sourceData); // <<< IDE!
+    //console.log(">>> DEBUG [useProcessedGraphData] sourceData:", sourceData); // <<< IDE!
     const { dataKey, sortOrder, displayLimit } = config || {}; // Biztonságos default értékek
 
     // DEBUG logolás
-    console.log(
-      `>>> DEBUG [useProcessedGraphData]: Hook running. config:`,
-      config,
-      `sourceData length: ${sourceData?.length ?? 0}`
-    );
+    // console.log(
+    //   `>>> DEBUG [useProcessedGraphData]: Hook running. config:`,
+    //   config,
+    //   `sourceData length: ${sourceData?.length ?? 0}`
+    // );
 
     if (!sourceData || sourceData.length === 0 || !config) {
-      console.log(
-        ">>> DEBUG [useProcessedGraphData]: Exiting early - no sourceData or config."
-      );
+      // console.log(
+      //   ">>> DEBUG [useProcessedGraphData]: Exiting early - no sourceData or config."
+      // );
       return [];
     }
 
@@ -42,18 +42,18 @@ const useProcessedGraphData = (sumObject, config) => {
 
     // 2. Filter: Kiszűrjük az érvényteleneket
     const filteredData = filterValidData(mappedData);
-    console.log(
-      `>>> DEBUG [useProcessedGraphData]: filteredData length: ${filteredData.length}`
-    );
+    // console.log(
+    //   `>>> DEBUG [useProcessedGraphData]: filteredData length: ${filteredData.length}`
+    // );
 
     // 3. Sort: Rendezés
     const sortedData = sortDataByParsedValue(filteredData, sortOrder);
 
     // 4. Limit: Levágás
     const limitedData = limitData(sortedData, displayLimit);
-    console.log(
-      `>>> DEBUG [useProcessedGraphData]: limitedData length after slice: ${limitedData.length}`
-    );
+    // console.log(
+    //   `>>> DEBUG [useProcessedGraphData]: limitedData length after slice: ${limitedData.length}`
+    // );
 
     return limitedData;
   }, [sumObject, config]); // Függőségek: sumObject és config
