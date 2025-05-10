f3719d17d5d97911e715abd60fc2c2cdf8c65241
nikolicsmilan <nikolicsmilan@gmail.com>
2025-05-10 23:23:55 +0200

    fix(ui): Update Error page text and GlowingButtons layout
    - Enhance descriptive text on the Error page.
    - Adjust layout of GlowingButtons on the Test page for better presentation.

--- src/pages/Test/components/GlowingButtons.jsx
+++ src/pages/Test/components/GlowingButtons.jsx
@@ -2,7 +2,7 @@ const GlowingButtons = () => {
   return (
     <div>
       <h1 className="text-primary text-xl mb-8 text-center">GlowingButtons</h1>
-      <div className="flex justify-center gap-4 my-10">
+      <div className="flex flex-col justify-center gap-4 my-10">
         <div className="w-48 h-32 rounded-xl shadow-xl backdrop-blur-md flex flex-col items-center justify-center p-4 cursor-pointer transition-all duration-300 
         ease-in-out relative overflow-hidden border border-[#22d3ee]/50 gradient-neutral-to-plasma hover:scale-105 hover:shadow-[0_0_15px_#22d3ee99]">
           {/* Belső tartalom (szöveg) */}
