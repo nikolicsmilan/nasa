ebb98b63172ab0d281409cecc014e842f202a94c
nikolicsmilan <nikolicsmilan@gmail.com>
2025-06-16 18:00:33 +0200

    feat: Disable morgan logging in test environment

--- backend/user-service/src/app.js
+++ backend/user-service/src/app.js
@@ -1,36 +1,43 @@
 // src/app.js
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const rfs = require('rotating-file-stream');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const rfs = require("rotating-file-stream");
+const path = require("path");
 
-const corsOptions = require('./config/corsOptions'); // Import CORS options
+const corsOptions = require("./config/corsOptions"); // Import CORS options
 //const authRoutes = require('./auth/authRoutes');   // Import auth routes
 //const userRoutes = require('./user/userRoutes');   // Import user routes
-const api = require('./routes/api');
+const api = require("./routes/api");
 
 const app = express();
 
 // Middlewares (apply before routes)
-// Create a rotating write stream
-const accessLogStream = rfs.createStream('access.log', {
-    interval: '1d', // Rotate daily
-    path: path.join(__dirname,  '..', 'logs', 'morgan') // Log directory
+if (process.env.NODE_ENV !== "test") {
+  // Create a rotating write stream
+  const accessLogStream = rfs.createStream("access.log", {
+    interval: "1d", // Rotate daily
+    path: path.join(__dirname, "..", "logs", "morgan"), // Log directory
   });
+  app.use(morgan("combined", { stream: accessLogStream }));
+}
 
-  // Setup the logger
-app.use(morgan('combined', { stream: accessLogStream }));
+// Setup the logger
+app.use(morgan("combined", { stream: accessLogStream }));
 app.use(cors(corsOptions)); // Apply CORS configuration
-app.use(express.json());    // Parse JSON request bodies
+app.use(express.json()); // Parse JSON request bodies
 app.use(express.urlencoded({ extended: false })); // Parse URL-encoded request bodies
 
 //Routes
 
 console.log("app.js: api middleware");
-app.use('/api', (req, res, next) => {
-  console.log("app.js: /api útvonalra érkezett kérés");
-  next(); // Fontos, hogy a next() függvényt meghívd
-}, api);
+app.use(
+  "/api",
+  (req, res, next) => {
+    console.log("app.js: /api útvonalra érkezett kérés");
+    next(); // Fontos, hogy a next() függvényt meghívd
+  },
+  api
+);
 
 module.exports = app;
